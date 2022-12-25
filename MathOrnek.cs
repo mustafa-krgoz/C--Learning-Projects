9700534dcffeb1c6f3ddf1365a31d8471fae95e5
Halit Mustafa Karagöz <93734376+mustafa-krgoz@users.noreply.github.com>
2022-12-25 23:00:30 +0300

    Add files via upload

---
+++ MathOrnek.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOrnek
+{
+     class MathOrnek
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            Console.Write("Bir sayı giriniz:");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Girilen sayının Mutlak değeri:" + Math.Abs(a));
+            Console.WriteLine("Girilen sayının Cosinüs değeri:" + Math.Cos(a*Math.PI/180));
+            Console.WriteLine("Girilen sayının Sinüs değeri:" + Math.Sin(a*Math.PI/180));
+            Console.WriteLine("Girilen sayının Yuvarlatılması:" + Math.Round(a));
+            Console.WriteLine("Girilen sayının Logaritması:" + Math.Log(a));
+            Console.WriteLine("Girilen sayının Karekökü:" + Math.Sqrt(a));
+            Console.WriteLine("Girilen sayının karesi:" + Math.Pow(a, 2));
+            Console.ReadLine();
+        }
+    }
+}
