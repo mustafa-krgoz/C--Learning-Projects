32ce9abe86c94c173367f617a1576b6b5d416a65
Halit Mustafa Karagöz <93734376+mustafa-krgoz@users.noreply.github.com>
2022-12-28 11:13:14 +0300

    Add files via upload

---
+++ KarmasikSayi1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KarmasikSayi
+{
+  class Program
+    {
+        static void Main(string[] args)
+        {
+            KarmasikSayi k = new KarmasikSayi(-5,-6); //Parametreli Yapıcı metoduna değer göndeririz.
+            k.Yaz();
+            Console.Read();
+        }
+    }
+}
