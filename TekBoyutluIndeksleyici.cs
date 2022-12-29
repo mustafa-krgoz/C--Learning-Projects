c457b147a61b7f0a7ed495d36cbfc89c8ab86cca
Halit Mustafa Karagöz <93734376+mustafa-krgoz@users.noreply.github.com>
2022-12-29 12:17:14 +0300

    Add files via upload

---
+++ TekBoyutluIndeksleyici.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TekBoyutluIndeksleyici
+{
+    class Sinif
+    {
+        public int Sayi;
+        public int this[int indeks]{
+            get { return Sayi; }
+            set { Sayi = value; }
+        }
+        static void Main(string[] args)
+        {
+            Sinif a = new Sinif();
+            a[5] = 30;
+            Console.WriteLine(a[5]);
+            Console.ReadLine();
+        }
+    }
+}
