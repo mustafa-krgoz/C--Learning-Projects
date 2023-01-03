4aaefbba705f9cfe391682d17551e1ff87b1c29d
Halit Mustafa Karagöz <93734376+mustafa-krgoz@users.noreply.github.com>
2023-01-03 09:26:20 +0300

    Add files via upload

---
+++ ArrayListSinifi.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraylistSınıfı
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ArrayList içerisindeki verilere erişmek
+            ArrayList liste = new ArrayList();
+            liste.Add("Murat");
+            liste.Add("Yeşim");
+            liste.Add("Deniz");
+
+            Console.WriteLine(liste[2]);
+            foreach (String eleman in liste)
+            {
+                Console.WriteLine(eleman);
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
