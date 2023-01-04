4c85b1e5bf7c0cc956b4e507dfe4c6f9d1b665bd
Halit Mustafa Karagöz <93734376+mustafa-krgoz@users.noreply.github.com>
2023-01-04 10:56:48 +0300

    Add files via upload

---
+++ ArrayMetot.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayMethod
+{
+    internal class Program
+    {
+        static void Yaz(Array dizi) // Bütün türlere ait dizileri yazmak istiyorsak kullanılır(Array)
+        {
+            foreach (object i in dizi) // object yerine var veya dynamic kullanılabilir.
+                Console.WriteLine(dizi);
+        }
+        static void Main(string[] args)
+        {
+            int[] dizi = { 1, 2, 4, 7, 9 };
+            Yaz(dizi);
+            Console.ReadLine();
+        }
+    }
+}
