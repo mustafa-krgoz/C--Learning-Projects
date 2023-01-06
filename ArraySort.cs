870644ba225f9e8e3daf452bc2db658abba27a96
Halit Mustafa Karagöz <93734376+mustafa-krgoz@users.noreply.github.com>
2023-01-06 08:58:58 +0300

    Add files via upload

---
+++ ArraySort.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraySort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Array.Sort();
+
+            string[] dizi = { "Zeynep", "Fatma", "Ali", "Yılmaz", "Gökhan", "Osman", "Feride" };
+            Console.WriteLine("Dizinin Elemanları\n==================");
+            foreach (string s in dizi)
+                Console.WriteLine(s);
+
+            Array.Sort(dizi);
+            Console.WriteLine();
+
+            Console.WriteLine("Sıralanmış Dizi\n===================");
+            foreach (string s in dizi)
+                Console.WriteLine(s);
+            Console.ReadLine();
+        }
+    }
+}
